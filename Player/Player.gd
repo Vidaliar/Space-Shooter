9d63f9fb1f0f7ecf54e4c4f60aac08b44d032c86
Vidaliar <97802043+Vidaliar@users.noreply.github.com>
2023-09-23 17:04:35 -0400

    Added bullets and asteroids
    made the ship violent

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,37 +1,43 @@
 extends CharacterBody2D
 
-var speed = 1
-var max_speed = 100
+var speed = 3.0
+var max_speed = 500
 var rotate_speed = 0.03
+var nose = Vector2(0,-60)
+var Bullet = load("res://Player/bullet.tscn")
 
 func _init():
 	pass
 func _process(delta):
 	pass
 func _physics_process(delta):
-	if Input.is_action_pressed("Rotate Left"):
-		rotation = rotation - rotate_speed
-	if Input.is_action_pressed("Rotate Right"):
-		rotation = rotation + rotate_speed
+	velocity += get_input() * speed
+	velocity = velocity.normalized() * clamp(velocity.length(), 0, max_speed)
+	position.x = wrapf(position.x, 0 , 1152)
+	position.y = wrapf(position.y,0,648) 
+	move_and_slide()
+	if Input.is_action_pressed("Fire"):
+		var Effects = get_node_or_null("/root/Game/Effects")
+		var bullet = Bullet.instantiate()
+		bullet.global_position = global_position + nose.rotated(rotation)
+		bullet.rotation = rotation
+		if Effects != null:
+			Effects.add_child(bullet)
+
+func get_input():
+	var to_return = Vector2.ZERO
+	$Exhaust.hide()
 	if Input.is_action_pressed("Forward"):
-		velocity = velocity + Vector2(0,-speed).rotated(rotation)
+		to_return += Vector2(0,-1)
+		$Exhaust.show()
+	if Input.is_action_pressed("Rotate Left"):
+		rotation -= rotate_speed
+	if Input.is_action_pressed("Rotate Right"):
+		rotation += rotate_speed
 	if Input.is_action_pressed("Backward"):
 		velocity = velocity + Vector2(0,speed).rotated(rotation)
 	if Input.is_action_pressed("Left"):
 		velocity = velocity + Vector2(-speed,0).rotated(rotation)
 	if Input.is_action_pressed("Right"):
 		velocity = velocity + Vector2(speed,0).rotated(rotation)
-	
-	position.x = wrapf(position.x, 0 , 1152)
-	position.y = wrapf(position.y,0,648) 
-	velocity = velocity.normalized() * clamp(velocity.length(),0,max_speed)
-	#if position.x > 1152:
-	#	position.x = 0
-	#if position.x < 0:
-	#	position.x = 1152
-	#if position.y > 648:
-	#	position.y = 0
-	#if position.y < 0:
-	#	position.y = 648
-		
-	move_and_slide()
+	return to_return.rotated(rotation) 
